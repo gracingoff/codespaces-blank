491842e86ec05bf02a63230e75f9b2ac67062fbf
gracingoff <ggoff1@uwyo.edu>
2024-09-17 22:15:21 +0000

    first commit

---
+++ lab02.py
@@ -0,0 +1,32 @@
+#Gracin Goff
+#UWYO COCS1010
+#9/25/24
+#Lab 02
+#Lab Section: 10
+#Sources:
+
+## Section 1 
+print("Hello COCS1010")
+
+hello_message = "Hello COSC1010"
+print(hello_message)
+
+mascot = "cowboy joe"
+print(mascot.title())
+
+UW = "University of Wyoming"
+year = "1886"
+print(f"The {UW} was founded in {year}")
+
+x= 5
+y= 10
+print(f" x + y = {x + y}")
+print(f" x - y = {x - y}")
+print(f" x * y = {x * y}")
+print(f" x / y = {x / y}")
+
+
+first_name = "Gracin"
+last_name = "Goff"
+space = " "
+print(first_name + space + last_name)
