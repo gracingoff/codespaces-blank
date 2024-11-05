a323c8f4be35d117575d5fe37caf7adef699fa88
gracingoff <ggoff1@uwyo.edu>
2024-11-05 21:18:56 +0000

    commit

--- goff_gracin-hw03.py
+++ goff_gracin-hw03.py
@@ -16,7 +16,6 @@ while True:
         break
     else:
 
-
         date = []
 
         date = date_input.split("/")
@@ -29,29 +28,51 @@ while True:
         day = int(day)
         year = int(year)
 
+
         y = year -1
 #Jan first falls on day x where:
-        weekday = (36 + y +(y/4) - (y/100) + (y/400))%7 
+        jan_1 = (36 + y +(y/4) - (y/100) + (y/400))%7
+
+        print(jan_1)
 
         months = {'Jan':'31','Feb':'28','Mar':'31','Apr':'30','May':'31','Jun':'30','Jul':'31','Aug':'31','Sep':'30','Oct':'31','Nov':'30','Dec':'31'}
 
         days_of_week = {'0':'Sunday','1':'Monday','2':'Tuesday','3':'Wednesday','4':'Thursday','5':'Friday','6':'Saturday'}
 
-        if 0 >= weekday < 1:
-            print(f'{date_input} is on {days_of_week['0']}')
-        elif 1 >= weekday < 2:
-            print(f'{date_input} is on {days_of_week['1']}')
-        elif 2 >= weekday < 3:
-            print(f'{date_input} is on {days_of_week['2']}')
-        elif 3 >= weekday < 4:
-            print(f'{date_input} is on {days_of_week['3']}')
-        elif 4 >= weekday < 5:
-            print(f'{date_input} is on {days_of_week['4']}')
-        elif 5 >= weekday < 5:
-            print(f'{date_input} is on {days_of_week['5']}')
-        else:
-            print(f'{date_input} is on {days_of_week['6']}')
+        if 0 >= jan_1 < 1:
 
+         print(f'{date_input} is on {days_of_week['0']}')
+
+        elif 1 >= jan_1 <2 :
+
+            print(f'{date_input} is on {days_of_week['1']}')
+
+        elif 2 >= jan_1 < 3:
+
+            print(f'{date_input} is on {days_of_week['2']}')
+
+        elif 3 >= jan_1 < 4:
+
+            print(f'{date_input} is on {days_of_week['3']}')
+
+        elif 4 >= jan_1 < 5:
+
+            print(f'{date_input} is on {days_of_week['4']}')
+
+        elif 5 >= jan_1 < 5:
+
+            print(f'{date_input} is on {days_of_week['5']}')
+
+        else:
+
+            print(f'{date_input} is on {days_of_week['6']}')
+        
+
+
+    
+
+
+    
 
 
 
