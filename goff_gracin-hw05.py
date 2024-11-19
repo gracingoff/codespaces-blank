a1b13f5188e44667973273a48421bb7586edf6b7
gracingoff <ggoff1@uwyo.edu>
2024-11-19 23:06:13 +0000

    push

--- goff_gracin-hw05.py
+++ goff_gracin-hw05.py
@@ -8,5 +8,21 @@
 # comments
 # here
 
-wb = open.pyxl.Workbook()
-print(wn)
+
+import openpyxl 
+from openpyxl.styles import Color , PatternFill
+import string 
+wb = openpyxl.workbook()
+sheet = wb.active 
+background = Color(rgb='ADD8E6')
+fill = PatternFill(patterntype = 'solid', fgColor = background)
+body = Color(rgb='00008B')
+fill_b = PatternFill(patterntype = 'solid', fgColor = body)
+eyes = Color(rgb='000000')
+fill_e = PatternFill(patterntype = 'solid', fgColor = eyes)
+white = Color(rgb='FFFFFF')
+fill_w = PatternFill(patterntype = 'solid', fgColor = white)
+mouth = Color(rgb = 'FF0000')
+fill_r = PatternFill(patterntype = 'solid', fgColor = mouth)
+
+cells_lb = ['A1','B2','C2','D2','E2','F2','G2','H2','I2','J2','K2','L2','M2','N2','O2','P2','Q2','R2','S2','T2','U2',]
