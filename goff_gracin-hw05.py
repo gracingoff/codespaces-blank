8ebcb206ae094ebbdb01cc8082a58706bdb7e89e
gracingoff <ggoff1@uwyo.edu>
2024-11-19 21:56:24 +0000

    push

---
+++ goff_gracin-hw05.py
@@ -0,0 +1,12 @@
+# Gracin Goff
+# UWYO COSC 1010
+# 11/19/2024
+# HW 05
+# Lab Section: 10
+# Sources, people worked with, help given to: 
+# your
+# comments
+# here
+
+wb = open.pyxl.Workbook()
+print(wn)
